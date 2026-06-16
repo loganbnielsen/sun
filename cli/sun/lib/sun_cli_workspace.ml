e0a3f36181b45f0f9f1f03df81e9f35bd6f9956d
lbendtly <lbendtlynielsen@gmail.com>
2026-06-16 10:26:48 -0600

    Revert "Merge branch 'REFAC-033/remove-dup-read-file'"
    This reverts commit c9e137a518f92612f7ac49f20b61b5a19b6c12f4, reversing
    changes made to 250970d0e6beda30095a140560bde1cf6e459393.

--- cli/sun/lib/sun_cli_workspace.ml
+++ cli/sun/lib/sun_cli_workspace.ml
@@ -15,6 +15,13 @@ let contains_string ~needle s =
   done;
   !found
 
+let read_file path =
+  let ic = open_in path in
+  let n = in_channel_length ic in
+  let s = really_input_string ic n in
+  close_in ic;
+  s
+
 (** Count .sql files in [dir]/db/migrations.  Returns 0 if the directory does
     not exist.  Used by [sun up] to warn users about unapplied migrations. *)
 let pending_migration_count ~dir =
@@ -41,7 +48,7 @@ let scan ~dir =
           let path = Filename.concat d entry in
           if entry = "dune" then begin
             (try
-              let content = Sun_cli_shell.read_file path in
+              let content = read_file path in
               if contains_string ~needle:"kafka_eio_service"  content then kafka      := true;
               if contains_string ~needle:"sun_storage"        content then postgres   := true;
               if contains_string ~needle:"obs_eio_loki"       content then loki       := true;
