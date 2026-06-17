08427bd76c67ee2046d28e2feb52a9f1ce87031c
lbendtly <lbendtlynielsen@gmail.com>
2026-06-17 11:07:57 -0600

    refac: flatten generated startup config

--- examples/pluto/app/payments/charge_svc/bin/main.ml
+++ examples/pluto/app/payments/charge_svc/bin/main.ml
@@ -1,13 +1,22 @@
+let env_nonempty name =
+  match Sys.getenv_opt name with
+  | Some value when value <> "" -> Some value
+  | _ -> None
+
+let optional_log_backend ~net ~clock = function
+  | None     -> Obs.stdout
+  | Some url ->
+    Obs_loki.create ~net ~clock ~url ~label_names:["service"; "team"] ()
+
+let optional_db_pool ~sw ~stdenv postgres_url =
+  Option.bind postgres_url (fun url ->
+    Db.create_pool ~url ~sw ~stdenv () |> Result.to_option)
+
 let () =
-  let postgres_url = Sys.getenv_opt "POSTGRES_URL" in
-  let loki_url     = Sys.getenv_opt "LOKI_URL" in
+  let postgres_url = env_nonempty "POSTGRES_URL" in
+  let loki_url     = env_nonempty "LOKI_URL" in
   Eio_main.run @@ fun env ->
-  let log_backend = match loki_url with
-    | None     -> Obs.stdout
-    | Some url ->
-      Obs_loki.create ~net:env#net ~clock:env#clock ~url
-        ~label_names:["service"; "team"] ()
-  in
+  let log_backend = optional_log_backend ~net:env#net ~clock:env#clock loki_url in
   let prom, render = Obs_prometheus.create () in
   let ot =
     Obs.with_context
@@ -16,11 +25,5 @@ let () =
       [("team", "payments")]
   in
   Eio.Switch.run @@ fun sw ->
-  let pool = match postgres_url with
-    | None     -> None
-    | Some url ->
-      (match Db.create_pool ~url ~sw ~stdenv:(env :> Caqti_eio.stdenv) () with
-       | Error _ -> None
-       | Ok p    -> Some p)
-  in
+  let pool = optional_db_pool ~sw ~stdenv:(env :> Caqti_eio.stdenv) postgres_url in
   Service.run (Handler.routes pool) ~env ~ot ~metrics_renderer:render ()
